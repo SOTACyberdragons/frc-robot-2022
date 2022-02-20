7e03185121685dce828ec94f5fe32780c0330137
madelinebelew <madeline.belew@gmail.com>
2022-02-20 14:42:57 -0800

    Added multiplexer support.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,11 @@
 package frc.robot;
 
+import com.revrobotics.Rev2mDistanceSensor.Unit;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.vision.VisionThread;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -13,12 +16,16 @@ import frc.robot.commands.DifferentialDriveWithJoysticks;
 import frc.robot.grip.CustomPipeline;
 import frc.robot.grip.GripPipeline;
 import frc.robot.subsystems.ShooterTest;
+import frc.robot.utils.MultiplexedDistanceSensor;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     public static RobotContainer m_robotContainer;
     public static ShooterTest m_shooterTest;
+
+    public static MultiplexedDistanceSensor m_leftSensor;
+    public static MultiplexedDistanceSensor m_rightSensor;
 
     @Override
     public void robotInit() {
@@ -41,21 +48,27 @@ public class Robot extends TimedRobot {
                 Constants.kTimeoutMs);
         RobotContainer.m_robotDrive.rightSlave.setSelectedSensorPosition(0, Constants.kPIDLoopIdx,
                 Constants.kTimeoutMs);
+
+        m_leftSensor = new MultiplexedDistanceSensor(I2C.Port.kOnboard, 7);
+        m_rightSensor = new MultiplexedDistanceSensor(I2C.Port.kOnboard, 6);
+
+        m_leftSensor.setAutomaticMode(true);
+        m_rightSensor.setAutomaticMode(true);
+        m_leftSensor.setDistanceUnits(Unit.kMillimeters);
+        m_rightSensor.setDistanceUnits(Unit.kMillimeters);
     }
 
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
+        SmartDashboard.putNumber("Left Range", m_leftSensor.getRange());
+        SmartDashboard.putNumber("Right Range", m_rightSensor.getRange());
     }
 
     @Override
     public void autonomousInit() {
         CommandScheduler.getInstance().cancelAll();
         CommandScheduler.getInstance().enable();
-
-        // RobotContainer.m_robotDrive.zeroHeading();
-        // RobotContainer.m_robotDrive.resetOdometry();
-        // RobotContainer.m_robotDrive.setCustomPose(45, 7.3, 1.95);
 
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
@@ -82,6 +95,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+
     }
 
     @Override
