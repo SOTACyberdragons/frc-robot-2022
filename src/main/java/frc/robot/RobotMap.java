86b69e0561f48905028bfb089eea6d8bbcbbba77
madelinebelew <madeline.belew@gmail.com>
2022-03-01 15:02:44 -0800

    Code cleanup.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,39 +1,73 @@
+//                                                 @                              
+//                                                  &@@                           
+//                          * .                    * @@@                          
+//                           * (@   ,                 @@@@                        
+//                               @@@*       /          @@@@                       
+//                                @@@@@@    @@(     ,* ,@@@@@                     
+//                         %@@@@/*  @@@@@@@@       ,**. @@@@@@                    
+//                      #********,    @@@@@@@@@@    ***  @@@@@@                   
+//                   **********    /    @@@@@@@@@@@@   ,  @@@@@@                  
+//                              &@@/  (@  (@@@@@@@@@@@@   @@@@@@@                 
+//                            @@@@@//  @@@@@@@@@@@@@@@@@@& @@@@@@@                
+//                          @@@@@@@//  @@@@@@@@# .@@@@@@@@@@@@@@@@                
+//                         @@@@@@&///  %@@@@@@@@(  *  @@@@@@@@@@@@                
+//                       *@@@@@//   @@@@@@@@@@@@@@%     @@@@@@@@@@@               
+//                      .@@@@@@@@@@//   .@@@@@@@@@@@@@@  @@@@@@@@@@@              
+//                      @@@@@@@@@@@@@@(/     @@@@@@@@@@@@@@@@@@@@@@@@@            
+//                   @ %@@@@@@@@@@@@@@   ,  @@@@@@@@@@@@@@@@@@@@@@@@@@@           
+//                  @@ @@@@@@@@@@@@@   .             *@@@@@@@@@  @@@@@@#          
+//                 @@@ @@@@@@@@@@@@%   *******@@@&///     &@@@@@@@@@@@@@          
+//                 @**  @@@@@@@@@@@   ******@@@@@@,          @@@@@@@@@@           
+//                 #*** @@@@@@@@@@@   *****@@@@@                  @@@@*           
+//                ***   @@@@@@@@@@@  ,****@@@,                                    
+//                 *      @@@@@@@@@@.  *****@@                                    
+//                          @@@@@@@@@#   ***%@                                    
+//                           ,@@@@@@@@@    ***@,  /                               
+//                              @@@@@@@@@(    ***   //////*.     */               
+//                                 //@@@@@@%/      *    ///////                   
+//                                 @    //////////                                
+//                                   @@**                                         
+//                                       @*****                                   
+//                                             *                                  
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 public class RobotMap {
-	//drivetrain
+	// drivetrain
 	public static final int RIGHT_MASTER = 2;
 	public static final int RIGHT_SLAVE = 3;
-	public static final int LEFT_MASTER = 1; 
-	public static final int LEFT_SLAVE = 0; 
+	public static final int LEFT_MASTER = 1;
+	public static final int LEFT_SLAVE = 0;
 
-	//spinner
+	// spinner
 	public static final int SPINNER_MOTOR = 7;
 
-	//shooter
-	public static final int LEFT_SHOOTER_MOTOR = 4 ;
+	// shooter
+	public static final int LEFT_SHOOTER_MOTOR = 4;
 	public static final int RIGHT_SHOOTER_MOTOR = 5;
 
-	
-
-	//robotintake
+	// robotintake
 	public static final int INTAKE_MOTOR = 6;
 	public static final int FEEDER_MOTOR = 7;
 
-	//hopper 
+	// hopper
 	public static final int HOPPER_MOTOR = 6;
-	
-	//PCM
+
+	// PCM
 	public static final int DOUBLE_SOLENOID_ZERO = 0;
 	public static final int DOUBLE_SOLENOID_ONE = 7;
 
-	//DIO 
+	// DIO
 	public static final int FEEDER_BREAKBEAM = 1;
 
-	//Pigeon IMU
+	// Pigeon IMU
 	public static final int PIGEON_IMU = 14;
 
-	//camera resolution
+	// camera resolution
 	public static final int IMG_WIDTH = 320;
-    public static final int IMG_HEIGHT = 240;
+	public static final int IMG_HEIGHT = 240;
 }
--- src/main/java/frc/robot/oi/limelightvision/limelight/LimeLight.java
+++ src/main/java/frc/robot/oi/limelightvision/limelight/LimeLight.java
@@ -7,10 +7,10 @@ import edu.wpi.first.wpilibj.Notifier;
 import frc.robot.oi.limelightvision.limelight.ControlMode.*;
 
 /**
-*   Lime Light Class was started by Corey Applegate of Team 3244
-*   Granite City Gearheads. We Hope you Enjoy the Lime Light
-*   Camera. 
-*/
+ * Lime Light Class was started by Corey Applegate of Team 3244
+ * Granite City Gearheads. We Hope you Enjoy the Lime Light
+ * Camera.
+ */
 public class LimeLight {
 
     private NetworkTable m_table;
@@ -19,7 +19,7 @@ public class LimeLight {
     private double _hearBeatPeriod = 0.1;
 
     class PeriodicRunnable implements java.lang.Runnable {
-	    public void run() {
+        public void run() {
             resetPilelineLatency();
             try {
                 Thread.sleep(50);
@@ -27,15 +27,16 @@ public class LimeLight {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-            if(getPipelineLatency()==0.0){
+            if (getPipelineLatency() == 0.0) {
                 isConnected = false;
-            }else{
+            } else {
                 isConnected = true;
             }
         }
     }
+
     Notifier _hearBeat = new Notifier(new PeriodicRunnable());
-   
+
     /**
      * Using the Default Lime Light NT table
      */
@@ -60,29 +61,32 @@ public class LimeLight {
     public LimeLight(NetworkTable table) {
         m_table = table;
         _hearBeat.startPeriodic(_hearBeatPeriod);
-       
+
     }
 
-    //This is a test
-    public boolean isConnected(){
+    // This is a test
+    public boolean isConnected() {
         return isConnected;
     }
 
     /**
-     * tv   Whether the limelight has any valid targets (0 or 1)
+     * tv Whether the limelight has any valid targets (0 or 1)
+     * 
      * @return
      */
     public boolean getIsTargetFound() {
         NetworkTableEntry tv = m_table.getEntry("tv");
         double v = tv.getDouble(0);
-        if (v == 0.0f){
+        if (v == 0.0f) {
             return false;
-        }else {
+        } else {
             return true;
         }
     }
+
     /**
      * tx Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+     * 
      * @return
      */
     public double getdegRotationToTarget() {
@@ -90,17 +94,21 @@ public class LimeLight {
         double x = tx.getDouble(0.0);
         return x;
     }
+
     /**
      * ty Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
-     * @return 
+     * 
+     * @return
      */
     public double getdegVerticalToTarget() {
         NetworkTableEntry ty = m_table.getEntry("ty");
         double y = ty.getDouble(0.0);
         return y;
     }
+
     /**
      * ta Target Area (0% of image to 100% of image)
+     * 
      * @return
      */
     public double getTargetArea() {
@@ -108,8 +116,10 @@ public class LimeLight {
         double a = ta.getDouble(0.0);
         return a;
     }
+
     /**
      * ts Skew or rotation (-90 degrees to 0 degrees)
+     * 
      * @return
      */
     public double getSkew_Rotation() {
@@ -117,8 +127,11 @@ public class LimeLight {
         double s = ts.getDouble(0.0);
         return s;
     }
+
     /**
-     * tl The pipeline’s latency contribution (ms) Add at least 11ms for image capture latency.
+     * tl The pipeline’s latency contribution (ms) Add at least 11ms for image
+     * capture latency.
+     * 
      * @return
      */
     public double getPipelineLatency() {
@@ -127,25 +140,27 @@ public class LimeLight {
         return l;
     }
 
-    private void resetPilelineLatency(){
+    private void resetPilelineLatency() {
         m_table.getEntry("tl").setValue(0.0);
     }
-    //Setters
-    
+    // Setters
+
     /**
-     * LedMode  Sets limelight’s LED state
+     * LedMode Sets limelight’s LED state
      * 
-     *  kon
-     *  koff
-     *  kblink
+     * kon
+     * koff
+     * kblink
+     * 
      * @param ledMode
      */
-     public void setLEDMode(LedMode ledMode) {
+    public void setLEDMode(LedMode ledMode) {
         m_table.getEntry("ledMode").setValue(ledMode.getValue());
     }
 
     /**
      * Returns current LED mode of the Lime Light
+     * 
      * @return LedMode
      */
     public LedMode getLEDMode() {
@@ -154,21 +169,23 @@ public class LimeLight {
         LedMode mode = LedMode.getByValue(led);
         return mode;
     }
-    
+
     /**
-     * camMode  Sets limelight’s operation mode
+     * camMode Sets limelight’s operation mode
      * 
-     *  kvision
-     *  kdriver (Increases exposure, disables vision processing)
+     * kvision
+     * kdriver (Increases exposure, disables vision processing)
+     * 
      * @param camMode
      */
-    
+
     public void setCamMode(CamMode camMode) {
         m_table.getEntry("camMode").setValue(camMode.getValue());
     }
 
     /**
      * Returns current Cam mode of the Lime Light
+     * 
      * @return CamMode
      */
     public CamMode getCamMode() {
@@ -181,33 +198,35 @@ public class LimeLight {
     /**
      * pipeline Sets limelight’s current pipeline
      * 
-     * 0 . 9	Select pipeline 0.9
+     * 0 . 9 Select pipeline 0.9
      * 
      * @param pipeline
      */
-    /* public void setPipeline(Double pipeline) {
-        if(pipeline<0){
-            pipeline = 0.0;
-            throw new IllegalArgumentException("Pipeline can not be less than zero");
-        }else if(pipeline>9){
-            pipeline = 9.0;
-            throw new IllegalArgumentException("Pipeline can not be greater than nine");
-        }
-        m_table.getEntry("pipeline").setValue(pipeline);
-    } */
+    /*
+     * public void setPipeline(Double pipeline) {
+     * if(pipeline<0){
+     * pipeline = 0.0;
+     * throw new IllegalArgumentException("Pipeline can not be less than zero");
+     * }else if(pipeline>9){
+     * pipeline = 9.0;
+     * throw new IllegalArgumentException("Pipeline can not be greater than nine");
+     * }
+     * m_table.getEntry("pipeline").setValue(pipeline);
+     * }
+     */
 
-     /**
+    /**
      * pipeline Sets limelight’s current pipeline
      * 
-     * 0 . 9	Select pipeline 0.9
+     * 0 . 9 Select pipeline 0.9
      * 
      * @param pipeline
      */
     public void setPipeline(Integer pipeline) {
-        if(pipeline<0){
+        if (pipeline < 0) {
             pipeline = 0;
             throw new IllegalArgumentException("Pipeline can not be less than zero");
-        }else if(pipeline>9){
+        } else if (pipeline > 9) {
             pipeline = 9;
             throw new IllegalArgumentException("Pipeline can not be greater than nine");
         }
@@ -216,9 +235,10 @@ public class LimeLight {
 
     /**
      * Returns current Pipeling of the Lime Light
+     * 
      * @return Pipelinge
      */
-    public double getPipeline(){
+    public double getPipeline() {
         NetworkTableEntry pipeline = m_table.getEntry("pipeline");
         double pipe = pipeline.getDouble(0.0);
         return pipe;
@@ -226,23 +246,26 @@ public class LimeLight {
 
     /**
      * Returns current Pipeling of the Lime Light
+     * 
      * @return Pipelinge
      */
-    public Integer getPipelineInt(){
+    public Integer getPipelineInt() {
         NetworkTableEntry pipeline = m_table.getEntry("pipeline");
         Integer pipe = (int) pipeline.getDouble(0.0);
         return pipe;
     }
 
     /**
-     * stream   Sets limelight’s streaming mode
+     * stream Sets limelight’s streaming mode
      * 
      * kStandard - Side-by-side streams if a webcam is attached to Limelight
-     * kPiPMain - The secondary camera stream is placed in the lower-right corner of the primary camera stream
-     * kPiPSecondary - The primary camera stream is placed in the lower-right corner of the secondary camera stream
+     * kPiPMain - The secondary camera stream is placed in the lower-right corner of
+     * the primary camera stream
+     * kPiPSecondary - The primary camera stream is placed in the lower-right corner
+     * of the secondary camera stream
      * 
      * @param stream
-     */ 
+     */
     public void setStream(StreamType stream) {
         m_table.getEntry("stream").setValue(stream.getValue());
     }
@@ -254,12 +277,12 @@ public class LimeLight {
         return mode;
     }
 
-
     /**
      * snapshot Allows users to take snapshots during a match
      * 
      * kon - Stop taking snapshots
      * koff - Take two snapshots per second
+     * 
      * @param snapshot
      */
     public void setSnapshot(Snapshot snapshot) {
@@ -269,16 +292,18 @@ public class LimeLight {
     public Snapshot getSnapshot() {
         NetworkTableEntry snapshot = m_table.getEntry("snapshot");
         double snshot = snapshot.getDouble(0.0);
-        Snapshot mode = Snapshot.getByValue(snshot );        
+        Snapshot mode = Snapshot.getByValue(snshot);
         return mode;
     }
 
-    // *************** Advanced Usage with Raw Contours *********************   
+    // *************** Advanced Usage with Raw Contours *********************
 
     /**
-     * Limelight posts three raw contours to NetworkTables that are not influenced by your grouping mode. 
-     * That is, they are filtered with your pipeline parameters, but never grouped. X and Y are returned 
-     * in normalized screen space (-1 to 1) rather than degrees.	 * 
+     * Limelight posts three raw contours to NetworkTables that are not influenced
+     * by your grouping mode.
+     * That is, they are filtered with your pipeline parameters, but never grouped.
+     * X and Y are returned
+     * in normalized screen space (-1 to 1) rather than degrees. *
      */
 
     public double getAdvanced_RotationToTarget(Advanced_Target raw) {
@@ -298,22 +323,24 @@ public class LimeLight {
         double a = taRaw.getDouble(0.0);
         return a;
     }
-    
+
     public double getAdvanced_Skew_Rotation(Advanced_Target raw) {
         NetworkTableEntry tsRaw = m_table.getEntry("ts" + Integer.toString(raw.getValue()));
         double s = tsRaw.getDouble(0.0);
         return s;
     }
 
-    //Raw Crosshairs:
-    //If you are using raw targeting data, you can still utilize your calibrated crosshairs:
-    
-    public double[] getAdvanced_RawCrosshair(Advanced_Crosshair raw){
+    // Raw Crosshairs:
+    // If you are using raw targeting data, you can still utilize your calibrated
+    // crosshairs:
+
+    public double[] getAdvanced_RawCrosshair(Advanced_Crosshair raw) {
         double[] crosshars = new double[2];
         crosshars[0] = getAdvanced_RawCrosshair_X(raw);
         crosshars[1] = getAdvanced_RawCrosshair_Y(raw);
         return crosshars;
     }
+
     public double getAdvanced_RawCrosshair_X(Advanced_Crosshair raw) {
         NetworkTableEntry cxRaw = m_table.getEntry("cx" + Integer.toString(raw.getValue()));
         double x = cxRaw.getDouble(0.0);
