4cfc250528da196a52e3088771315f2274bc0927
walter298 <walterstein456@gmail.com>
2022-03-25 05:54:46 -0700

    atuo2

--- src/main/java/frc/robot/commands/AutoCommands/AutoSimple.java
+++ src/main/java/frc/robot/commands/AutoCommands/AutoSimple.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.commands.SpinIntake;
 
 public class AutoSimple extends SequentialCommandGroup {
     private static final Trajectory LEFT_1 = PathPlanner.loadPath("left-1", Constants.kMaxSpeedMetersPerSecond,
@@ -18,12 +19,15 @@ public class AutoSimple extends SequentialCommandGroup {
     public AutoSimple()
     {
         addCommands(
+            new ParallelDeadlineGroup(new WaitCommand(1), new SpinUpShooter("Low")),
+            new ParallelDeadlineGroup(new WaitCommand(1), new Shoot("Low")),
             new ParallelDeadlineGroup(new DrivetrainRamseteCommand(RobotContainer.m_drive, LEFT_1).robotRelative(),
+            new SpinIntake()),
             new DrivetrainRamseteCommand(RobotContainer.m_drive, LEFT_2),
-            new ParallelDeadlineGroup(new WaitCommand(1), new SpinUpShooter("High"))),
-            new ParallelDeadlineGroup(new WaitCommand(1), new Shoot("High")),
-            new ParallelDeadlineGroup(new WaitCommand(1), new Shoot("High")),
-            new DrivetrainRamseteCommand(RobotContainer.m_drive, LEFT_2)
+            new ParallelDeadlineGroup(new WaitCommand(1), new SpinUpShooter("Low")),
+            new ParallelDeadlineGroup(new WaitCommand(1), new Shoot("Low")),
+            new DrivetrainRamseteCommand(RobotContainer.m_drive, LEFT_1),
+            new DrivetrainRamseteCommand(RobotContainer.m_drive, LEFT_1)
             );
     }
 }
